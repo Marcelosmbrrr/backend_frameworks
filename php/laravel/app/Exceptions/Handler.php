c1879b280ae14930cde4dd3e7984853b3f3f09d9
Marcelo Moreira <profissionalsmbr@gmail.com>
2023-06-19 20:08:58 -0300

    backend laravel att

--- php/laravel/app/Exceptions/Handler.php
+++ php/laravel/app/Exceptions/Handler.php
@@ -7,24 +7,28 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (Throwable $e) {
+
+            $data = [
+                'data' => null,
+                'message' => $e->getMessage(),
+            ];
+
+            $code = $e->getCode() ?? 500;
+
+            return response()->json($data, $code);
         });
     }
 }
--- php/laravel/routes/api.php
+++ php/laravel/routes/api.php
@@ -2,18 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Authentication\AuthenticationController;
+use App\Http\Controllers\Modules\{
+    DashboardController,
+    UsersController,
+    RolesController
+};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post("/auth/signin", [AuthenticationController::class, "signIn"]);
+Route::post("/auth/signup", [AuthenticationController::class, "signUp"]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(["auth"])->group(function () {
+    Route::post("/auth/signout", [AuthenticationController::class, "signOut"]);
+    Route::get("/auth/refresh-data", [AuthenticationController::class, "refreshAndVerifyAuthentication"]);
+    Route::get("/dashboard", DashboardController::class);
+    Route::resource("/users", UsersController::class);
+    Route::resource("/roles", RolesController::class);
 });
