46f880389b11f434497faa1e2fa70b06f7319d97
Marcelo Moreira <profissionalsmbr@gmail.com>
2023-06-23 20:36:30 -0300

    python remotion

--- python/django/roles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("/update/<int:role_id>", views.index, name="update"),
-    path("/delete/<int:role_id>", views.index, name="delete"),
-]
