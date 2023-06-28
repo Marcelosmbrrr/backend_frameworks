0d3f8344e4a7e6cf83cc2a5a5f93ba53f536d814
Marcelo Moreira <profissionalsmbr@gmail.com>
2023-06-28 14:51:04 -0300

    python remotion

--- python/django-rest-framework/roles/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from roles.models import Role
-
-class RoleSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Role
-        fields = ['id', 'name']
